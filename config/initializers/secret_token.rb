f32b6f45c79513f9f47c10d96dd71421287f21d4
shane carr <devolverdesign@gmail.com>
2014-09-10 17:43:05 +0100

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pinrails::Application.config.secret_token = '11f953a513f29a26139f21f482bb5ac2c7eb9a4c1372e432b12b07bcbae2768cb6188ea20fd79225790432bf01aec37d50d8122e7c4a560efbfea263615df3d6'
